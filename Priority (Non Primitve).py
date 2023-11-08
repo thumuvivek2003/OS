4d886c8eae9a6951531f84b448efaa12b1d01eae
thumuvivek2003 <107809171+thumuvivek2003@users.noreply.github.com>
2023-11-08 16:58:45 +0530

    Update Priority (Non Primitve).py

--- Priority (Non Primitve).py
+++ Priority (Non Primitve).py
@@ -25,14 +25,14 @@ for i in range(n):
     if i == 0:
         ct[0] = at[0]+bt[0]
     else:
-        minIdx = i
+        maxIdx = i
         for j in range(i+1,n):
-            if(at[j]<=ct[i-1] and pr[j]>pr[minIdx]):
-                minIdx = j
-        at[minIdx],at[i] = at[i],at[minIdx]
-        bt[minIdx],bt[i] = bt[i],bt[minIdx]
-        pr[minIdx],pr[i] = pr[i],pr[minIdx]
-        pid[minIdx],pid[i] = pid[i],pid[minIdx]
+            if(at[j]<=ct[i-1] and pr[j]>pr[maxIdx]):
+                maxIdx = j
+        at[maxIdx],at[i] = at[i],at[maxIdx]
+        bt[maxIdx],bt[i] = bt[i],bt[maxIdx]
+        pr[maxIdx],pr[i] = pr[i],pr[maxIdx]
+        pid[maxIdx],pid[i] = pid[i],pid[maxIdx]
         ct[i] = bt[i]+max(ct[i-1],at[i])
     tat[i] = ct[i]-at[i]
     wt[i] = tat[i]-bt[i]
